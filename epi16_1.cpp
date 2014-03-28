63c82caaceb434e41430d6042cc1e1546c66a7d2
brandon <yangrucheng@gmail.com>
2014-03-28 02:02:39 -0400

    16-1 maze

--- epi16_1.cpp
+++ epi16_1.cpp
@@ -1,103 +1,160 @@
 #include <vector>
 #include <queue>
-#include  <iostream>
+#include <iostream>
 #include <cmath>
+#include <stack>
 
 
 using namespace std;
 
-typedef enum {UNVISITED, VISITED, BLOCKED, ENTRY, EXIT} Status;
+typedef enum {UNVISITED, VISITED, BLOCKED} Status;
 
 class Coordinate {
 public:
     int x;
     int y;
     Status status;
+    char symbol;
 public:
     Coordinate() {
         status = UNVISITED;
+        symbol = 'O';
     };
-    Coordinate(int x_, int y_) : x(x_), y(y_), status(UNVISITED) {}
+    Coordinate(int x_, int y_) : x(x_), y(y_), status(UNVISITED), symbol('O')  {}
 };
 
 class Maze {
 public:
     int dimension;
     vector<vector<Coordinate> > board;
-    Coordinate entrance;
+    int entryX, entryY;
+    int exitX, exitY;
     Coordinate exit;
+    stack<pair<int, int> > path;
+    
 public:
     Maze(int mazeSize) {
         dimension = mazeSize;
         board.resize(dimension);
         for (int i = 0; i < mazeSize; i++) {
-            board[i].resize(dimension);
             for (int j = 0; j < dimension; j++) {
-                boardi[i].push_back(Coordinate())
+                board[i].push_back(Coordinate(i, j));
+            }
         }
     }
-    void setBlock(Coordinate block);
-    void setEntrance(Coordinate entrance_);
-    void setExit(Coordinate exit_);
+    void setBlock(int ,int );
+    void setEntry(int, int);
+    void setExit(int, int);
     void display();
-    bool checkNeighbor(Coordinate c1, Coordinate c2);
+    bool isLegal(int ,int ,int, int);
+    void searchPath();
+    bool DFS(int x, int y);
 };
 
 
-void Maze::setEntrance(Coordinate entrance_) {
-    entrance.x = entrance_.x;
-    entrance.y = entrance_.y;
-    board[entrance.x][entrance.y] = 'A';
+void Maze::setEntry(int x_, int y_) {
+    entryX = x_;
+    entryY = y_;
+    board[x_][y_].symbol = 'A';
 }
 
-void Maze::setExit(Coordinate exit_) {
-    exit.x = exit_.x;
-    exit.y = exit_.y;
-    board[exit.x][exit.y] = 'B';
+void Maze::setExit(int x_, int y_) {
+    exitX = x_;
+    exitY = y_;
+    board[x_][y_].symbol = 'B';
 }
 
-void Maze::setBlock(Coordinate block_) {
-    board[block_.x][block_.y] = 'X';
+void Maze::setBlock(int x_, int y_) {
+    board[x_][y_].status = BLOCKED;
+    board[x_][y_].symbol = 'X';
 }
 
-bool Maze::checkNeighbor(Coordinate c1, Coordinate c2) {
-    if (c1.x < 0 && c1.y < 0 && c1.x >= dimension && c1.y >= dimension && 
-        c2.x < 0 && c2.y < 0 && c2.x >= dimension && c2.y >= dimension)
+bool Maze::isLegal(int x1, int y1, int x2, int y2) {
+    
+    if (x1 < 0 || y1 < 0 || x1 >= dimension || y1 >= dimension || 
+        x2 < 0 || y2 < 0 || x2 >= dimension || y2 >= dimension)
         return false;
-    if (c1.x == c2.x && c1.y == c2.y) 
+    if (x1 == x2 && y1 == y2) 
         return false;
-    if (abs(c1.x - c2.x) > 1 || abs(c1.y - c2.y) > 1)
+    if (abs(x1 - x2) > 1 || abs(y1 - y2) > 1)
         return false;
-    if (board[c2.x][c2.y] == 'X')
+    if (board[x2][y2].status == BLOCKED)
         return false;
     return true;
-
 }
 
 void Maze::display() {
     cout << endl;
     for (int row = dimension - 1; row >= 0; row--) {
         for (int col = 0; col < dimension; col++) {
-            cout << board[row][col] << " ";
+            cout << board[row][col].symbol << " ";
         }
         cout << endl;
     }
     cout << endl;
 }
 
+void Maze::searchPath() {
+    cout << "size " << path.size() << endl;
+    if (DFS(entryX, entryY)) {
+        cout << "Successful!" << endl;
+        cout << "The path is: " << endl;
+        while(!path.empty()) {
+            pair<int, int> temp = path.top();
+            path.pop();
+            cout << temp.first << " " << temp.second << endl;
+        }
+    }
+    
+}
+
+bool Maze::DFS(int x, int y) {
+    //cout << "Visiting " << x << " " << y << endl;
+    if (x == exitX && y == exitY) {
+        path.push(pair<int,int>(x,y));
+        return true;
+    }
+
+    bool result;
+
+    board[x][y].status = VISITED;
+    vector<pair<int, int> > legalMove;
+
+    for(int up = -1; up <= 1; up++) {
+        for (int right = -1; right <= 1; right++) {
+            int step = abs(up) + abs(right);
+            if (step == 1 && isLegal(x, y, x + up, y + right)) {
+                legalMove.push_back(pair<int ,int>(x+up,y+right));
+            }
+        }
+    }
+    for (auto move : legalMove) {
+        if (board[move.first][move.second].status == UNVISITED)
+            if (DFS(move.first, move.second)){
+                path.push(pair<int, int>(x,y));
+                return true;
+            }
+    }
+    return false; 
+}
 
 
 int main() {
 
     //Construct maze
     Maze M(4);
-    M.setEntrance(Coordinate(0,0));
-    M.setExit(Coordinate(3,3));
-    M.setBlock(Coordinate(0,1));
-    M.setBlock(Coordinate(1,1));
-    M.setBlock(Coordinate(3,0));
-    M.setBlock(Coordinate(3,2));
- 
+    M.setEntry(0,0);
+    M.setExit(3,3);
+    M.setBlock(0,1);
+    M.setBlock(1,1);
+    //M.setBlock(2,1);
+    //M.setBlock(3,1);
+    M.setBlock(3,0);
+    M.setBlock(3,2);
+    
     M.display();
+
+    M.searchPath();
+
     return 0;
 }
